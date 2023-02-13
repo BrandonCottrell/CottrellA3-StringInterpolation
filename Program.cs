d01b11c9a83892b6a4629e35178fee8b9e42fd84
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-02-13 17:06:16 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CottrellA3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+
+            Console.WriteLine($"{now,40:MMMM dd,yyyy}");
+            Console.WriteLine($"{now:yyyy.MM.dd}");
+            Console.WriteLine($"Day {now:dd} of {now:MMMM,yyyy}");
+            Console.WriteLine($"Year:{now:yyyy},Month:{now:MM},Day:{now:dd}");
+            Console.WriteLine($"{now,10:dddd}");
+            Console.WriteLine($"{now,10:t}{now,10:dddd}");
+            Console.WriteLine($"h:{now:hh},m:{now:mm},s:{now:ss}");
+            Console.WriteLine($"{now:yyyy.MM.dd}.{now:hh.mm.ss}");
+
+            Console.WriteLine("");
+
+            var pi = Math.PI;
+
+            Console.WriteLine($"{pi:C2}");
+            Console.WriteLine($"{pi,15:N3}");
+
+
+
+        }
+    }
+}
